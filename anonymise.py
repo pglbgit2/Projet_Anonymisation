80693e874672926efbd77db572b7c1e71ac610ab
pglbgit2 <pierre.gilles.le.botlan@gmail.com>
2023-10-25 18:18:58 +0200

    modification pour correspondre format donnée

--- anonymise.py
+++ anonymise.py
@@ -1,27 +1,39 @@
 import CSVmanager
-from faker import Faker
+#from faker import Faker
 import random
-faker = Faker('fr_FR')
+from datetime import datetime, timedelta
+import string
 
-def anonymise(fileToReadName, fileToWriteName):
-    tab = CSVmanager.readTabCSVFile(fileToReadName)
-    for value in tab:
-        value[0] = faker.first_name()
-        value[1] = faker.last_name()
-    random.shuffle(tab)
-    CSVmanager.writeTabCSVFile(tab,fileToWriteName)
+#faker = Faker('fr_FR')
+
+def generateString(longueur):
+    autorizedChar = string.ascii_letters + string.digits  
+    generatedString = ''.join(random.choice(autorizedChar) for _ in range(longueur))
+    return generatedString
 
 def generate_csv(filename, nbvalues):
     ensemble_de_valeurs = []
     for _ in range(nbvalues):
-        prenom = faker.first_name()
-        nom = faker.last_name()
-        annee_naissance = random.randint(1980, 2023)
-        mois_naissance = random.randint(1, 12)
-        ville_naissance = faker.city()
-
-        ensemble_de_valeurs.append([prenom, nom, annee_naissance, mois_naissance, ville_naissance])
+        id_user = generateString(5)
+        date_debut = datetime(year=2023, month=1, day=1)
+        date_fin = datetime(year=2023, month=12, day=31)
+        date = date_debut + timedelta(days=random.randint(0, (date_fin - date_debut).days))
+        for _ in range(random.randint(3,12)):
+            latitude = random.uniform(3.686, 3.688)
+            longitude = random.uniform(43, 44)
+            ensemble_de_valeurs.append([id_user, date, latitude, longitude])
     CSVmanager.writeTabCSVFile(ensemble_de_valeurs, filename)
 
-#generate_csv('tableau.csv', 300)
+def anonymise(fileToReadName, fileToWriteName):
+    tab = CSVmanager.readTabCSVFile(fileToReadName)
+    for value in tab:
+        value[0] = generateString(5)
+        value[2] += random.uniform(0.0001, 0.0002)
+        value[3] += random.uniform(0.001, 0.001)
+    random.shuffle(tab)
+    CSVmanager.writeTabCSVFile(tab,fileToWriteName)
+
+
+
+#generate_csv('tableau.csv', 50)
 anonymise("tableau.csv", "anonymisedTab.csv")
