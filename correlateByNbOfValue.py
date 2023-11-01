9a5fbba0cacfe1b7bd39689f3d46b8bf7a586ec5
pglbgit2 <pierre.gilles.le.botlan@gmail.com>
2023-11-01 21:18:58 +0100

    CorrelateById marche sur bdd reduite

--- correlateByNbOfValue.py
+++ correlateByNbOfValue.py
@@ -1,25 +1,26 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import count, col
-from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType, TimestampType
+from pyspark.sql.functions import monotonically_increasing_id
 
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+import CSVManager
 # Initialiser SparkSession
-spark = SparkSession.builder.appName("Comparaison d'identifiants").getOrCreate()
+spark = SparkSession.builder.appName("Correlation Analysis").getOrCreate()
 
 # Définir le schéma de la donnée
 schema = StructType([
-    StructField("identifiant", IntegerType(), True),
+    StructField("identifiant", StringType(), True),
     StructField("timestamp", TimestampType(), True),
     StructField("longitude", DoubleType(), True),
     StructField("latitude", DoubleType(), True)
 ])
 print(">after schema definition")
+# Charger les fichiers CSV avec le schéma spécifié
+original_df = spark.read.csv("tableau.csv", schema=schema, header=True).withColumn("Ogrow_number", monotonically_increasing_id()+2)
+print(">after reading original file")
 
 
-original_df = spark.read.csv("tableau.csv", schema=schema, header=True)
-modified_df = spark.read.csv("anonymisedTab.csv", schema=schema, header=True)
-print(">after reading file")
-
+modified_df = spark.read.csv("anonymisedTab.csv", schema=schema, header=True).withColumn("Anonymrow_number", monotonically_increasing_id()+2)
+print(">after reading anonymized file")
 
 # Arrêter la session Spark
 spark.stop()
-
