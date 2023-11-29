0532f07a48d976df28be2cdf974e852a7fd32450
rtas-vadum <agrand630@gmail.com>
2023-11-29 20:42:54 +0100

    la fonction d'anonymisation est ok, reste à reprendre la fonction de pseudonymisation

--- cleanAnonymizeByNightDay.py
+++ cleanAnonymizeByNightDay.py
@@ -2,7 +2,7 @@ import random
 import string
 from functools import reduce
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, hour, randn, col, weekofyear, monotonically_increasing_id
+from pyspark.sql.functions import avg, hour, randn, col, weekofyear, monotonically_increasing_id, expr, row_number
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 import CSVManager
 import pandas
@@ -28,32 +28,45 @@ def readfile(path: str):
     ])
     print(">after schema definition")
     return spark.read.csv(path, header=False, schema=schema, sep='\t').withColumn("numero_ligne", monotonically_increasing_id())
+    
 
 def anonymize_but_not_completely(startOfTheDay, startOfTheNight, df, variation, path):
     dfDay = df.filter((hour(df["timestamp"]) >= startOfTheDay) & (hour(df["timestamp"]) < startOfTheNight))
     dfNight = df.filter((hour(df["timestamp"]) < startOfTheDay) | (hour(df["timestamp"]) >= startOfTheNight))
     print(">after day and night dataframe")
 
-    dfDay = dfDay.agg(weekofyear("timestamp").alias("week"))
+    dfDay = dfDay.withColumn("week", weekofyear("timestamp"))
+    dfNight = dfNight.withColumn("week", weekofyear("timestamp"))
+    
     print(">adding week")
+
 
     avg_jour = dfDay.groupBy("id","week").agg(avg("longitude").alias("avg_longitude"), avg("latitude").alias("avg_latitude"))
     avg_nuit = dfNight.groupBy("id","week").agg(avg("longitude").alias("avg_longitude"), avg("latitude").alias("avg_latitude"))
+    # avg_jour.coalesce(1).write.csv("test_jour", header=True,  mode="overwrite", sep="\t")
+    # avg_jour.coalesce(1).write.csv("test_nuit", header=True,  mode="overwrite", sep="\t")
+
     print(">after average location")
 
-    values_jour = dfDay.join(avg_jour, ["id", "timestamp"]).withColumn("new_longitude", col("avg_longitude") + randn(seed=23)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
-    values_nuit = dfNight.join(avg_nuit, ["id", "timestamp"]).withColumn("new_longitude", col("avg_longitude") + randn(seed=34)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
+    values_jour = dfDay.join(avg_jour, ["id", "week"]).withColumn("new_longitude", col("avg_longitude") + randn(seed=23)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude")
+    values_nuit = dfNight.join(avg_nuit, ["id", "week"]).withColumn("new_longitude", col("avg_longitude") + randn(seed=34)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude")
+
+    values_jour.show()
+
+    # values_jour = dfDay.join(avg_jour, (dfDay.id == avg_jour.id) & (dfDay.week == avg_jour.week)).withColumn("new_longitude", col("avg_longitude") + randn(seed=23)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
+    # values_nuit = dfNight.join(avg_nuit, (dfNight.id == avg_nuit.id) & (dfNight.week == avg_nuit.week)).withColumn("new_longitude", col("avg_longitude") + randn(seed=34)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
     print(">after generating new values")
 
     ready_to_be_anonymised = values_jour.union(values_nuit)
-    ready_to_be_anonymised = ready_to_be_anonymised.withColumn("longitude", col("new_longitude"))
-    ready_to_be_anonymised = ready_to_be_anonymised.withColumn("latitude", col("new_latitude"))
+    # ready_to_be_anonymised = ready_to_be_anonymised.withColumn("longitude", col("new_longitude"))
+    # ready_to_be_anonymised = ready_to_be_anonymised.withColumn("latitude", col("new_latitude"))
     print(">after union")
 
  
     # CSVManager.writeTabCSVFile(ready_to_be_anonymised.toPandas(), path)
     final = ready_to_be_anonymised.withColumn("timestamp", expr("substring(timestamp, 1, length(timestamp)-3) || ':00'"))
-    final.coalesce(1).write.csv(path, header=False,  mode="overwrite", sep="\t")
+    final_sorted = final.sort("numero_ligne", ascending=[True])
+    final_sorted.coalesce(1).write.csv(path, header=True,  mode="overwrite", sep="\t")
 
 # def pseudonymize(fileToReadName, fileToWriteName):
 #     tab = CSVManager.readTabCSVFile(fileToReadName)
@@ -70,6 +83,7 @@ def anonymize_but_not_completely(startOfTheDay, startOfTheNight, df, variation, 
 
 if __name__=='__main__':
     df = readfile("ReferenceINSA.csv")
+    df.show()
     print(">after reading original file")
     anonymize_but_not_completely(6, 22, df, 0.0001, "nopseudo.csv")
     # pseudonymize("nopseudo.csv", "anonymFrangipane.csv")
