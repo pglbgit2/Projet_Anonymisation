0bc59d24b03dc05a71552fae487d80720cda63a2
GGravys-S <simeon.gravis@insa-cvl.fr>
2023-11-30 09:05:29 +0100

    Started weekend POI

--- cleanAnonymizeByNightDay.py
+++ cleanAnonymizeByNightDay.py
@@ -2,7 +2,7 @@ import random
 import string
 from functools import reduce
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, hour, randn, col, weekofyear, monotonically_increasing_id, expr, row_number
+from pyspark.sql.functions import avg, hour, dayofweek, randn, col, weekofyear, monotonically_increasing_id, expr, row_number
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 import CSVManager
 import pandas
@@ -79,7 +79,59 @@ def anonymize_but_not_completely(startOfTheDay, startOfTheNight, df, variation, 
 #         else:
 #             value[0] = idAno[value[0]]
 #     CSVManager.writeTabCSVFile(tab,fileToWriteName)
-    
+
+def anonymiseDayNightWeekend(startOfTheDay, startOfTheNight, df, variation, path):
+    dfDay = df.filter((1 < (dayofweek(df["timestamp"])) <= 6) & ((hour(df["timestamp"]) >= startOfTheDay) & (hour(df["timestamp"]) < startOfTheNight)))
+    dfNight = df.filter((1 < (dayofweek(df["timestamp"])) <= 6) & ((hour(df["timestamp"]) < startOfTheDay) | (hour(df["timestamp"]) >= startOfTheNight)))
+    dfWeekend = df.filter(((dayofweek(df["timestamp"])) == 1 | (dayofweek(df["timestamp"])) == 7) & ((hour(df["timestamp"]) >= startOfTheDay) & (hour(df["timestamp"]) < startOfTheNight)))
+    # dayofweek() -> 1 for sunday through 7 for a saturday
+
+    print("> After day, night and weekend dataframes")
+
+    dfDay = dfDay.withColumn("week", weekofyear("timestamp"))
+    dfNight = dfNight.withColumn("week", weekofyear("timestamp"))
+    dfWeekend = dfWeekend.withColumn("week", weekofyear("timestamp"))
+
+    print("> Adding week")
+
+    avg_jour = dfDay.groupBy("id", "week").agg(avg("longitude").alias("avg_longitude"),
+                                               avg("latitude").alias("avg_latitude"))
+    avg_nuit = dfNight.groupBy("id", "week").agg(avg("longitude").alias("avg_longitude"),
+                                                 avg("latitude").alias("avg_latitude"))
+    avg_weekend = dfWeekend.groupBy("id", "week").agg(avg("longitude").alias("avg_longitude"),
+                                                 avg("latitude").alias("avg_latitude"))
+    # avg_jour.coalesce(1).write.csv("test_jour", header=True,  mode="overwrite", sep="\t")
+    # avg_jour.coalesce(1).write.csv("test_nuit", header=True,  mode="overwrite", sep="\t")
+
+    print("> After average location")
+
+    values_jour = dfDay.join(avg_jour, ["id", "week"]).withColumn("new_longitude", col("avg_longitude") + randn(
+        seed=23) * variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42) * variation).drop(
+        "avg_longitude", "avg_latitude")
+    values_nuit = dfNight.join(avg_nuit, ["id", "week"]).withColumn("new_longitude", col("avg_longitude") + randn(
+        seed=34) * variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42) * variation).drop(
+        "avg_longitude", "avg_latitude")
+    values_weekend = dfWeekend.join(avg_weekend, ["id","week"]).withColumn("new_longitude", col("avg_longitude") + randn(
+        seed=26) * variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42) * variation).drop(
+        "avg_longitude", "avg_latitude")
+
+    values_jour.show()
+
+    # values_jour = dfDay.join(avg_jour, (dfDay.id == avg_jour.id) & (dfDay.week == avg_jour.week)).withColumn("new_longitude", col("avg_longitude") + randn(seed=23)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
+    # values_nuit = dfNight.join(avg_nuit, (dfNight.id == avg_nuit.id) & (dfNight.week == avg_nuit.week)).withColumn("new_longitude", col("avg_longitude") + randn(seed=34)*variation).withColumn("new_latitude", col("avg_latitude") + randn(seed=42)*variation).drop("avg_longitude", "avg_latitude", "longitude", "latitude")
+    print(">after generating new values")
+
+    ready_to_be_anonymised = values_jour.union(values_nuit)
+    # ready_to_be_anonymised = ready_to_be_anonymised.withColumn("longitude", col("new_longitude"))
+    # ready_to_be_anonymised = ready_to_be_anonymised.withColumn("latitude", col("new_latitude"))
+    print(">after union")
+
+    # CSVManager.writeTabCSVFile(ready_to_be_anonymised.toPandas(), path)
+    final = ready_to_be_anonymised.withColumn("timestamp",
+                                              expr("substring(timestamp, 1, length(timestamp)-3) || ':00'"))
+    final_sorted = final.sort("numero_ligne", ascending=[True])
+    final_sorted.coalesce(1).write.csv(path, header=True, mode="overwrite", sep="\t")
+
 
 if __name__=='__main__':
     df = readfile("ReferenceINSA.csv")
