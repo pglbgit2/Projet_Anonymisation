0ed5c29fd022abd6b0828762bdefe2239fa70998
qlemeur <quentin.le_meur@insa-cvl.fr>
2023-12-15 01:40:39 +0100

    2eme étage assembler ?

--- moyenne_jour.py
+++ moyenne_jour.py
@@ -48,12 +48,13 @@ if __name__ == '__main__':
     dfDef_avg = calcule_avgCoord(dfDef)
 
     # Renommer les colonnes avant de faire la jointure
-    dfVic_avg = dfVic_avg.withColumnRenamed("window", "vic_window").withColumnRenamed("avg_longitude", "vic_avg_longitude").withColumnRenamed("avg_latitude", "vic_avg_latitude").withColumnRenamed("id", "vic_id").withColumnRenamed("date", "vic_date")
-    dfDef_avg = dfDef_avg.withColumnRenamed("window", "def_window").withColumnRenamed("avg_longitude", "def_avg_longitude").withColumnRenamed("avg_latitude", "def_avg_latitude").withColumnRenamed("id", "def_id").withColumnRenamed("date", "def_date")
+    dfVic_avg = dfVic_avg.withColumnRenamed("window", "vic_window").withColumnRenamed("avg_longitude", "vic_avg_longitude").withColumnRenamed("avg_latitude", "vic_avg_latitude").withColumnRenamed("id", "vic_id")
+    dfDef_avg = dfDef_avg.withColumnRenamed("window", "def_window").withColumnRenamed("avg_longitude", "def_avg_longitude").withColumnRenamed("avg_latitude", "def_avg_latitude").withColumnRenamed("id", "def_id").withColumnRenamed("date", "def_date").withColumnRenamed("hour", "def_hour").withColumnRenamed("day", "def_day")
 
     # Joindre les deux DataFrames sur la date, l'heure et le jour
-    dfJoined = dfDef_avg.join(dfVic_avg, (dfDef_avg.def_date == dfVic_avg.vic_date) & (dfDef_avg.hour == dfVic_avg.hour) & (dfDef_avg.day == dfVic_avg.day), "inner")    
-    dfJoined.show(500)
+    dfJoined = dfDef_avg.join(dfVic_avg, (dfDef_avg.def_date == dfVic_avg.date) & (dfDef_avg.def_hour == dfVic_avg.hour) & (dfDef_avg.def_day == dfVic_avg.day), "inner")   
+    dfJoined = dfJoined.drop("def_date").drop("def_day").drop("def_hour").drop("def_window")
+    #dfJoined.filter(dfJoined.def_id==1).filter(dfJoined.hour==1).filter(dfJoined.day==1).show(500)
 
     # Calculer la distance entre les moyennes des coordonnées GPS
     dfJoined = dfJoined.withColumn("distance", ((dfJoined.vic_avg_longitude - dfJoined.def_avg_longitude) ** 2 + (dfJoined.vic_avg_latitude - dfJoined.def_avg_latitude) ** 2) ** 0.5)
@@ -62,15 +63,17 @@ if __name__ == '__main__':
     dfJoined = dfJoined.withColumn("week", weekofyear(dfJoined["vic_window.start"]))
 
     # Calculer le rang de chaque ligne en fonction de la distance, pour chaque semaine et chaque "def_id"
-    windowSpec = Window.partitionBy("week", "def_id").orderBy("distance")
+    windowSpec = Window.partitionBy("hour", "def_id", "day", "week").orderBy("vic_window.start").orderBy("distance")
     dfJoined = dfJoined.withColumn("rank", row_number().over(windowSpec))
+    dfJoined.show(100)
 
-    # Définir la fenêtre de partitionnement
-    windowSpec = Window.partitionBy("def_id", "week").orderBy("vic_window.start")
+    # # Définir la fenêtre de partitionnement
+    # windowSpec = Window.partitionBy("def_id", "week").orderBy("vic_window.start")
 
-    # Ajouter une colonne "rank" qui donne le rang de chaque ligne dans sa partition
-    dfJoined = dfJoined.withColumn("rank", row_number().over(windowSpec))
-    dfJoined.show(500)
+    # # Ajouter une colonne "rank" qui donne le rang de chaque ligne dans sa partition
+    # dfJoined = dfJoined.withColumn("rank", row_number().over(windowSpec))
+    # dfJoined.show(100)
+
 
     # Sélectionner uniquement les lignes avec le rang 1, c'est-à-dire la distance minimale pour chaque semaine et chaque "vic_id"
     dfMinDistance = dfJoined.filter(dfJoined.rank == 1)
@@ -80,11 +83,11 @@ if __name__ == '__main__':
 
     # Compter la fréquence de chaque "def_id" pour chaque "vic_id"
     dfCount = dfMinDistance.groupBy("vic_id", "def_id", "week").count()
-
+    dfCount.orderBy("week").orderBy("def_id").show(100)
     # Trier par fréquence et sélectionner l'ID le plus fréquent pour chaque "vic_id"
     windowSpec2 = Window.partitionBy("def_id", "week").orderBy(dfCount["count"].desc())
     dfMostFrequent = dfCount.withColumn("rank", row_number().over(windowSpec2)).filter(col("rank") == 1)
-
+    dfMostFrequent.orderBy("week").orderBy("def_id").show(100)
     # Sélectionner uniquement les colonnes nécessaires
     dfFinal = dfMostFrequent.select("vic_id", "def_id", "week")
 
