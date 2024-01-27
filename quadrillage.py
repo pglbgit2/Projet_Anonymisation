58d637f83c4c6c0d0a7c50db5395594d4ea0f0e8
GGravys-S <simeon.gravis@insa-cvl.fr>
2024-01-27 11:25:48 +0100

    Correction Frequentation 1

--- quadrillage.py
+++ quadrillage.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 import pyspark.sql.functions as F
+import pandas as pd
 from pyspark.sql.functions import filter
 import itertools
 
@@ -20,15 +21,15 @@ def readfile(path: str):
     return spark.read.csv(path, header=False, schema=schema, sep='\t')
 
 # on prend deux points qui servent de reference pour le tracé: ils délimitent les points en haut à gauche et en bas à droite d'un rectangle
-def Quadrillage(x1,y1,x2,y2,precision=0.1):
+def Quadrillage(x1,y1,x2,y2,precision=0.05):
     nx = round(abs(x2-x1)/precision)
     tabx = []
-    x = min([x1,x2])
+    x = round(min([x1,x2]),round(1/precision))
     for i in range(nx):
         tabx.append(x+i*precision)
     ny = round(abs(y2-y1)/precision)
     taby = []
-    y = min([y1,y2])
+    y = round(min([y1,y2]),round(1/precision))
     for j in range(ny):
         taby.append(y+j*precision)
     return (tabx,taby)
@@ -38,35 +39,60 @@ def round_quadrillage(row, tab): # row: [longitude / latitude] selon ce qu'on ch
 
 def closest_without_border(v,tab):
     if min(tab)<=v<=max(tab):
-        min(tab, key=lambda x: abs(x-v))
+        return min(tab, key=lambda x: abs(x-v))
     else:
         return -1
 
-
-
 def get_dict_from_df(df, tabx, taby):
-    df = df.withColumn("timestamp", F.round((F.unix_timestamp("timestamp")/1200)*1200).cast("timestamp"))
-    print(">rounding timestamp to 20 minutes gap")
+    # df = df.withColumn("timestamp", F.round((F.round(F.unix_timestamp("timestamp")/60)/20)*1200).cast("timestamp"))
+    df = df.withColumn("timestamp", F.expr("substring(timestamp, 1, length(timestamp)-4) || '0:00'"))
+
+    print(">rounding timestamp to 10 minutes gap")
     round_longitude_udf = F.udf(lambda z: round_quadrillage(z, taby), DoubleType())
     round_latitude_udf = F.udf(lambda z: round_quadrillage(z, tabx), DoubleType())
+
     df = df.withColumn('longitude', round_longitude_udf(F.array('longitude')))
+    # df.show(5)
+
     df = df.filter(F.col("longitude") != -1)
+    # df.show(5)
+
     df = df.withColumn('latitude', round_latitude_udf(F.array('latitude')))
+    # df.show(5)
+
     df = df.filter(F.col("latitude") != -1)
+    # df.show(5)
+
     df = df.groupBy("timestamp","longitude","latitude").agg(F.count("*").alias("nbValue"))
+    # df.show(5)
+
     df = df.dropDuplicates()
-    print(">after count")        
+    # df.show(5)
+    print(">after count")
     results = {} # dictionnaire par position
-    for position in itertools.product(tabx,taby):
-        results[position] = {row['timestamp']:row['nbValue'] for row in df.collect()}
+    for row in df.collect():
+        if (row[1],row[2]) not in results.keys():
+            results[(row[1],row[2])] = dict()
+        results[(row[1],row[2])][row[0]] = row[3]
     print(">after putting in dictionnary")
     return results
 
+def metric_compare_dict(dicAnon,dicOrigin):
+    # dic be like : results[position]-> {timestamp:nbValue}
+    for resultA in dicAnon:
+        pass
+    for resultO in dicOrigin:
+        pass
+
 if __name__ == '__main__':
-    ogdf = readfile("../ReferenceINSA.csv")
+    (tabx, taby) = Quadrillage(45.850, 4.730, 45.623, 5.020)
+    ogdf = readfile("../default.csv")
     print(">after read original file")
-    (tabx, taby) = Quadrillage(45.850, 4.730, 45.623 , 5.020)
     og_dict = get_dict_from_df(ogdf,tabx,taby)
+    #print(og_dict)
+    # andf = readfile("../Anon.csv")
+    # print(">after read anonymise file")
+    # an_dict = get_dict_from_df(andf,tabx,taby)
 
 
 
